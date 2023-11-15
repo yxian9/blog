60c66ed34a75383e487125be3fae465e8df08e14
yu-xiang <yux.uth@gmail.com>
2023-11-15 09:54:10 -0800

    update comment section using react component

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt(), react()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
